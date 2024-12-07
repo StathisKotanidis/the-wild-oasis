23ffe784a6a9b29126eff53eee6648f3463d3992
StathisKotanidis <stathiskotanidhs@gmail.com>
2024-12-07 12:39:58 +0200

    uploaded an image to backend

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://hzoyaxzijespjlithyjc.supabase.co";
+export const supabaseUrl = "https://hzoyaxzijespjlithyjc.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6b3lheHppamVzcGpsaXRoeWpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE4MzMzNTQsImV4cCI6MjA0NzQwOTM1NH0.GAWs68ktdt4msj1ZyxiBXICR7BhjOwggZcIrkq9vbBA";
 const supabase = createClient(supabaseUrl, supabaseKey);
